5fcc48c5e2fc9e7985ceffbc09c33de0f231648d
EC2 Default User <ec2-user@ip-172-31-3-113.ec2.internal>
2018-10-13 08:22:19 +0000

    post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+	before_action :require_user_logged_in , only:[:new, :create, :destroy]
+	before_action :correct_user, only:[:destroy]
+	
+	def index
+		@posts = Post.all.order('created_at DESC').page(params[:page])
+	end
+	
+	def show
+		@post = Post.find(params[:id])
+	end
+	
+	def new
+		@post = current_user.posts.build
+	end
+	
+	def create
+		@post = current_user.posts.build(post_params)
+    
+		if @post.save
+			flash[:success] = '投稿しました'
+			redirect_to root_url
+		else
+			@post.destroy
+			flash.now[:danger] = '投稿に失敗しました'
+			render 'new'
+		end
+	end
+	
+	def destroy
+		@post.destroy
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_back(fallback_location: root_path)
+	end
+	
+	private
+	
+	def post_params
+		params.require(:post).permit(:content,:title, :picture)
+	end
+	
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    unless @post
+      redirect_to root_url
+    end
+  end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  mount_uploader :picture, PictureUploader
+  belongs_to :user
+  validates :title, presence: true, length:{maximum: 50}
+  validates :content, presence: true, length:{maximum: 255}
+end
